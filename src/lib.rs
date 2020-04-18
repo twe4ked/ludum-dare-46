2f05bf3144dad5dd0fbbd3753625f12691a3b5db
Odin Dutton <odindutton@gmail.com>
2020-04-18 14:55:47 +1000

    Set up panic handler

--- src/lib.rs
+++ src/lib.rs
@@ -68,6 +68,8 @@ impl State {
 
 #[wasm_bindgen(start)]
 pub fn start() {
+    std::panic::set_hook(Box::new(console_error_panic_hook::hook));
+
     let f = Rc::new(RefCell::new(None));
     let g = f.clone();
 
