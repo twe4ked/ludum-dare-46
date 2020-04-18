fd249f899278d62687bb43da057542810c8d107b
Odin Dutton <odindutton@gmail.com>
2020-04-18 12:25:52 +1000

    Add game loop
    https://rustwasm.github.io/docs/wasm-bindgen/examples/request-animation-frame.html

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,38 @@
+use std::cell::RefCell;
 use std::f64;
+use std::rc::Rc;
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsCast;
 
+fn window() -> web_sys::Window {
+    web_sys::window().expect("no global `window` exists")
+}
+
+fn request_animation_frame(f: &Closure<dyn FnMut()>) {
+    window()
+        .request_animation_frame(f.as_ref().unchecked_ref())
+        .expect("should register `requestAnimationFrame` OK");
+}
+
+fn document() -> web_sys::Document {
+    window()
+        .document()
+        .expect("should have a document on window")
+}
+
+fn body() -> web_sys::HtmlElement {
+    document().body().expect("document should have a body")
+}
+
+struct State {
+    context: web_sys::CanvasRenderingContext2d,
+}
+
 #[wasm_bindgen(start)]
 pub fn start() {
+    let f = Rc::new(RefCell::new(None));
+    let g = f.clone();
+
     let document = web_sys::window().unwrap().document().unwrap();
     let canvas = document.get_element_by_id("canvas").unwrap();
     let canvas: web_sys::HtmlCanvasElement = canvas
@@ -18,28 +47,50 @@ pub fn start() {
         .dyn_into::<web_sys::CanvasRenderingContext2d>()
         .unwrap();
 
-    context.begin_path();
+    let state = State { context };
 
-    // Draw the outer circle.
-    context
-        .arc(75.0, 75.0, 50.0, 0.0, f64::consts::PI * 2.0)
-        .unwrap();
+    *g.borrow_mut() = Some(Closure::wrap(Box::new(move || {
+        state.context.clear_rect(
+            0.,
+            0.,
+            state.context.canvas().unwrap().width() as f64,
+            state.context.canvas().unwrap().height() as f64,
+        );
 
-    // Draw the mouth.
-    context.move_to(110.0, 75.0);
-    context.arc(75.0, 75.0, 35.0, 0.0, f64::consts::PI).unwrap();
+        state.context.begin_path();
 
-    // Draw the left eye.
-    context.move_to(65.0, 65.0);
-    context
-        .arc(60.0, 65.0, 5.0, 0.0, f64::consts::PI * 2.0)
-        .unwrap();
+        // Draw the outer circle.
+        state
+            .context
+            .arc(75.0, 75.0, 50.0, 0.0, f64::consts::PI * 2.0)
+            .unwrap();
 
-    // Draw the right eye.
-    context.move_to(95.0, 65.0);
-    context
-        .arc(90.0, 65.0, 5.0, 0.0, f64::consts::PI * 2.0)
-        .unwrap();
+        // Draw the mouth.
+        state.context.move_to(110.0, 75.0);
+        state
+            .context
+            .arc(75.0, 75.0, 35.0, 0.0, f64::consts::PI)
+            .unwrap();
 
-    context.stroke();
+        // Draw the left eye.
+        state.context.move_to(65.0, 65.0);
+        state
+            .context
+            .arc(60.0, 65.0, 5.0, 0.0, f64::consts::PI * 2.0)
+            .unwrap();
+
+        // Draw the right eye.
+        state.context.move_to(95.0, 65.0);
+        state
+            .context
+            .arc(90.0, 65.0, 5.0, 0.0, f64::consts::PI * 2.0)
+            .unwrap();
+
+        state.context.stroke();
+
+        // Schedule ourself for another requestAnimationFrame callback.
+        request_animation_frame(f.borrow().as_ref().unwrap());
+    }) as Box<dyn FnMut()>));
+
+    request_animation_frame(g.borrow().as_ref().unwrap());
 }
